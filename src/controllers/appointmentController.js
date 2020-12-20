f86685be33f20c935c59c85df547acea44ab2f06
İrem Ekser <iremekser@gmail.com>
2020-12-21 00:49:09 +0300

    cancel and populate added

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -4,8 +4,8 @@ const appointmentEnums = require('./../enums/appointmentEnums');
 
 exports.create = async (req, res) => {
     try {
-        const newQuery = await pool.query("insert into have_appointment( patient_id, doctor_id,appointment_date,appointment_time,loc,description ) values($1,$2,$3,$4,$5,$6) RETURNING *",
-            [req.body.patient_id, req.body.doctor_id, req.body.appointment_date, req.body.appointment_time, req.body.loc, req.body.description]);
+        const newQuery = await pool.query("insert into have_appointment( patient_id, doctor_id,appointment_date,appointment_time,loc,description, is_cancelled ) values($1,$2,$3,$4,$5,$6,$7) RETURNING *",
+            [req.body.patient_id, req.body.doctor_id, req.body.appointment_date, req.body.appointment_time, req.body.loc, req.body.description, false]);
         return res.status(200).json({
             message: appointmentEnums.CREATED,
             newQuery
@@ -39,9 +39,9 @@ exports.delete = async (req, res) => {
 
 exports.update = async (req, res) => {
     try {
-        const { patient_id, doctor_id, appointment_date, appointment_time, loc, description } = req.body;
-        const appointment = await pool.query("UPDATE have_appointment set patient_id =$1, doctor_id=$2,appointment_date=$3,appointment_time=$4,loc=$5,description=$6 where appointment_id = $7 RETURNING *",
-            [patient_id, doctor_id, appointment_date, appointment_time, loc, description, req.params.appointment_id]);
+        const { patient_id, doctor_id, appointment_date, appointment_time, loc, description, is_cancelled } = req.body;
+        const appointment = await pool.query("UPDATE have_appointment set patient_id =$1, doctor_id=$2,appointment_date=$3,appointment_time=$4,loc=$5,description=$6, is_cancelled = $7 where appointment_id = $8 RETURNING *",
+            [patient_id, doctor_id, appointment_date, appointment_time, loc, description, is_cancelled, req.params.appointment_id]);
         if (!appointment) {
             return res.status(404).json({
                 message: appointmentEnums.NOT_FOUND
@@ -58,9 +58,36 @@ exports.update = async (req, res) => {
     }
 };
 
+exports.cancel = async (req, res) => {
+    try {
+        const app = await pool.query(`update have_appointment
+        set is_cancelled = $1
+        where appointment_id = $2
+        `, [req.body.is_cancelled, req.params.appointment_id])
+        return res.status(200).json({
+            message: appointmentEnums.IS_CANCELLED_UPDATED,
+            appointment: app.rows
+        });
+    } catch (error) {
+        return res.status(500).json({
+            ...error
+        });
+    }
+}
+
 exports.find = async (req, res) => {
     try {
-        const appointment = await pool.query("SELECT * from have_appointment WHERE appointment_id = $1", [req.params.appointment_id]);
+        const appointment = await pool.query(`SELECT * from have_appointment
+            left join patient p on ha.patient_id = p.patient_id
+            left join doctor d on ha.doctor_id = d.doctor_id
+            WHERE appointment_id = $1`, [req.params.appointment_id]);
+        console.log(appointment);
+
+        if (!appointment) {
+            return res.status(404).json({
+                message: appointmentEnums.NOT_FOUND
+            });
+        }
         return res.status(200).json({
             message: appointmentEnums.FOUND,
             appointment: appointment.rows
@@ -75,8 +102,23 @@ exports.find = async (req, res) => {
 
 exports.list = async (req, res) => {
     try {
-        const appointments = await pool.query("SELECT * from have_appointment");
-        return res.status(200).json({ appointments: appointments.rows });
+        let appointments;
+        if (req.query.tc_no) {
+            appointments = await pool.query(`SELECT * FROM have_appointment a
+            left join person p on p.tc_no = $1
+            left join doctor d on a.doctor_id = d.doctor_id
+            left join person p2 on d.tc_no = p2.tc_no
+            left join clinic c  on d.clinic_id = c.clinic_id
+            where a.patient_id = (SELECT patient_id FROM patient pp WHERE pp.tc_no=$1)`, [req.query.tc_no]);
+        }
+        else
+            appointments = await pool.query(`SELECT * from have_appointment ha
+            left join patient p on ha.patient_id = p.patient_id
+            left join doctor d on ha.doctor_id = d.doctor_id
+            `);
+        let ascAppointments = appointments.rows.sort((a, b) => a.appointment_date - b.appointment_date);
+
+        return res.status(200).json({ appointments: ascAppointments });
 
     } catch (error) {
         return res.status(500).json({
